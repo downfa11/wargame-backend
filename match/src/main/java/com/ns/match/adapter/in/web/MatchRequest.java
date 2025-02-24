51a56425e9343d9ad8b6885389c7386853224fc9
downfa11 <downfa11@naver.com>
2025-02-25 04:36:53 +0900

    refactor: MatchRequest의 Builder 패턴 남용 개선

--- match/src/main/java/com/ns/match/adapter/in/web/MatchRequest.java
+++ match/src/main/java/com/ns/match/adapter/in/web/MatchRequest.java
@@ -1,14 +1,8 @@
 package com.ns.match.adapter.in.web;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class MatchRequest {
     private Long membershipId;
 }
