103e907d0f96c2eaeb9825b5cd17da0a8db47b77
downfa11 <downfa11@naver.com>
2025-04-20 13:16:55 +0900

    feat: MySQL 초기화시 테이블 생성 쿼리문 개선

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE champion_stats
 (
-    champion_id    INT PRIMARY KEY,
+    id    INT AUTO_INCREMENT PRIMARY KEY,
     name           VARCHAR(255) UNIQUE,
     maxhp          INT,
     maxmana        INT,
@@ -23,14 +23,14 @@ CREATE TABLE champion_stats
     platoon_count  INT
 );
 
-INSERT INTO champion_stats (champion_id, name, maxhp, maxmana, attack, absorptionRate, defense, movespeed, maxdelay,
+INSERT INTO champion_stats (name, maxhp, maxmana, attack, absorptionRate, defense, movespeed, maxdelay,
                             attspeed, attrange, critical, criProbability, growHp, growMana, growAtt, growCri,
                             growCriPob, unit_type, platoon_type, platoon_count)
-VALUES (0, 'toy0', 580, 200, 60, 0.0, 5, 10.0, 3.0, 0.8, 12, 30.0, 50.0, 100, 30, 10, 10, 0, 'Regiment', 'Archer', 3),
-       (1, 'toy1', 500, 200, 50, 0.0, 5, 10.0, 3.0, 0.8, 25, 100.0, 10.0, 100, 30, 10, 10, 0, 'Brigade', 'Infantry', 4),
-       (2, 'Spearman', 620, 200, 0, 5.0, 80, 10.0, 2.0, 1.0, 10, 50.0, 30.0, 100, 30, 10, 10, 0, 'Regiment', 'Infantry', 3),
-       (3, 'Lux', 490, 200, 53, 0.0, 5, 10.0, 2.0, 1.0, 10, 10.0, 0.0, 100, 30, 10, 10, 0, 'Brigade', 'Infantry', 3),
-       (4, 'Yasuo', 490, 200, 60, 0.0, 5, 10.0, 2.0, 1.0, 10, 10.0, 0.0, 100, 30, 10, 10, 0, 'Regiment', 'Infantry', 3);
+VALUES ('toy0', 580, 200, 60, 0.0, 5, 10.0, 3.0, 0.8, 12, 30.0, 50.0, 100, 30, 10, 10, 0, 'Regiment', 'Archer', 3),
+       ('toy1', 500, 200, 50, 0.0, 5, 10.0, 3.0, 0.8, 25, 100.0, 10.0, 100, 30, 10, 10, 0, 'Brigade', 'Infantry', 4),
+       ('Spearman', 620, 200, 0, 5.0, 80, 10.0, 2.0, 1.0, 10, 50.0, 30.0, 100, 30, 10, 10, 0, 'Regiment', 'Infantry',3),
+       ('Lux', 490, 200, 53, 0.0, 5, 10.0, 2.0, 1.0, 10, 10.0, 0.0, 100, 30, 10, 10, 0, 'Brigade', 'Infantry', 3),
+       ('Yasuo', 490, 200, 60, 0.0, 5, 10.0, 2.0, 1.0, 10, 10.0, 0.0, 100, 30, 10, 10, 0, 'Regiment', 'Infantry', 3);
 
 CREATE TABLE item_stats
 (
@@ -55,18 +55,22 @@ VALUES ('Sword of the Brave', 100, 50, 20, 10.0, 0.0, 5.0, 10, 0.1, 3),
        ('Boots of Swiftness', 80, 0, 0, 15.0, 0.0, 0.0, 0, 0.0, 0),
        ('Ring of Power', 150, 0, 30, 0.0, 0.0, 0.0, 20, 0.0, 0);
 
+
 CREATE TABLE unit_stats
 (
-    unit_id       INT AUTO_INCREMENT PRIMARY KEY,
-    champion_id   INT,
-    unit_kind     ENUM('Infantry', 'Archer') NOT NULL,
-    maxhp         INT   NOT NULL,
-    curhp         INT   NOT NULL,
-    attrange      INT   NOT NULL,
-    attack        INT   NOT NULL,
-    maxdelay      FLOAT NOT NULL,
-    speed         INT   NOT NULL,
-    soldier_count INT   NOT NULL,
-    unit_type     ENUM('Brigade', 'Regiment') NOT NULL,
-    FOREIGN KEY (champion_id) REFERENCES wargame.champion_stats (champion_id)
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(20),
+    maxhp    INT   NOT NULL,
+    attrange INT   NOT NULL,
+    attack   INT   NOT NULL,
+    maxdelay FLOAT NOT NULL,
+    speed    INT   NOT NULL
 );
+
+
+INSERT INTO unit_stats (name, maxhp, attrange, attack, maxdelay, speed)
+VALUES ('Toy0 unit', 580, 12, 60, 3.0, 10),
+       ('Toy1 unit', 500, 25, 50, 3.0, 9),
+       ('Spearman unit', 620, 10, 0, 2.0, 7),
+       ('Lux unit', 490, 10, 53, 2.0, 6),
+       ('Yasuo unit', 490, 10, 60, 2.0, 7);
