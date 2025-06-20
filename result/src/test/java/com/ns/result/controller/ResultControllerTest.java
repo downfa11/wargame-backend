7f4156d3ffd96039c6958ebcc2146d02ce1aa684
downfa11 <downfa11@naver.com>
2025-06-20 23:25:46 +0900

    test: 전적 조회시 클라이언트 요청에 맞춰서 MatchDto 반환 테스트

--- result/src/test/java/com/ns/result/controller/ResultControllerTest.java
+++ result/src/test/java/com/ns/result/controller/ResultControllerTest.java
@@ -1,10 +1,12 @@
 package com.ns.result.controller;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
+import static reactor.core.publisher.Mono.when;
 
 import com.ns.common.ClientRequest;
+import com.ns.common.TimelineData;
+import com.ns.result.adapter.in.web.MatchDto;
+import com.ns.result.adapter.in.web.ResultController;
 import com.ns.result.adapter.out.persistence.elasticsearch.Result;
 import com.ns.result.application.port.in.FindResultUseCase;
 import com.ns.result.application.port.in.RegisterResultUseCase;
@@ -18,17 +20,17 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@WebFluxTest(ResultControllerTest.class)
+@WebFluxTest(ResultController.class)
 public class ResultControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
 
     @MockBean private RegisterResultUseCase registerResultUseCase;
-
     @MockBean private FindResultUseCase findResultUseCase;
 
     private Result result;
+    private MatchDto matchDto;
 
     @BeforeEach
     public void init() {
@@ -39,85 +41,76 @@ public class ResultControllerTest {
                 .room(101)
                 .winTeam("blue")
                 .loseTeam("red")
-                .blueTeams(List.of(ClientRequest.builder().build(), ClientRequest.builder().build()))
-                .redTeams(List.of(ClientRequest.builder().build(), ClientRequest.builder().build()))
+                .blueTeams(List.of(ClientRequest.builder().nickname("A").build()))
+                .redTeams(List.of(ClientRequest.builder().nickname("B").build()))
                 .dateTime("2025-02-01T12:00:00Z")
                 .gameDuration(120)
+                .timelineData(List.of(TimelineData.builder().time(1).blueGold(1000).redGold(900).build()))
                 .build();
+
+        matchDto = MatchDto.from(result);
     }
 
     @Test
-    public void 모든_전적_목록을_조회하는_메서드() {
-        // given
-        Flux<Result> resultFlux = Flux.just(result);
-        when(findResultUseCase.getResultList()).thenReturn(resultFlux);
+    void 모든_전적_목록을_조회하는_메서드() {
+        when(findResultUseCase.getResultList()).thenReturn(Flux.just(result));
 
-        // when
         webTestClient.get().uri("/v1/result/list")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBodyList(Result.class)
+                .expectBodyList(MatchDto.class)
                 .hasSize(1)
-                .contains(result);
-        // then
+                .contains(matchDto);
+
         verify(findResultUseCase, times(1)).getResultList();
     }
 
     @Test
-    public void 사용자의_이름으로_전적_결과를_조회하는_메서드() {
-        // given
+    void 사용자의_이름으로_전적_결과를_조회하는_메서드() {
         String name = "Test Name";
         int offset = 0;
-        Flux<Result> resultFlux = Flux.just(result);
-        when(findResultUseCase.getGameResultsByName(name, offset)).thenReturn(resultFlux);
+        when(findResultUseCase.getGameResultsByName(name, offset)).thenReturn(Flux.just(result));
 
-        // when
         webTestClient.get().uri(uriBuilder -> uriBuilder.path("/v1/result/search/name/{name}")
                         .queryParam("offset", offset)
                         .build(name))
                 .exchange()
                 .expectStatus().isOk()
-                .expectBodyList(Result.class)
+                .expectBodyList(MatchDto.class)
                 .hasSize(1)
-                .contains(result);
-        // then
+                .contains(matchDto);
+
         verify(findResultUseCase, times(1)).getGameResultsByName(name, offset);
     }
 
     @Test
-    public void membershipId로_전적_결과를_조회하는_메서드() {
-        // given
+    void membershipId로_전적_결과를_조회하는_메서드() {
         Long membershipId = 123L;
         int offset = 0;
-        Flux<Result> resultFlux = Flux.just(result);
-        when(findResultUseCase.getGameResultsByMembershipId(membershipId, offset)).thenReturn(resultFlux);
+        when(findResultUseCase.getGameResultsByMembershipId(membershipId, offset)).thenReturn(Flux.just(result));
 
-        // when
         webTestClient.get().uri(uriBuilder -> uriBuilder.path("/v1/result/search/id/{membershipId}")
                         .queryParam("offset", offset)
                         .build(membershipId))
                 .exchange()
                 .expectStatus().isOk()
-                .expectBodyList(Result.class)
+                .expectBodyList(MatchDto.class)
                 .hasSize(1)
-                .contains(result);
-        // then
+                .contains(matchDto);
+
         verify(findResultUseCase, times(1)).getGameResultsByMembershipId(membershipId, offset);
     }
 
     @Test
-    public void 테스트를_위해_더미_전적을_생성하는_메서드() {
-        // given
-        Mono<Result> resultMono = Mono.just(result);
-        when(registerResultUseCase.createResultTemp()).thenReturn(resultMono);
+    void 테스트를_위해_더미_전적을_생성하는_메서드() {
+        when(registerResultUseCase.createResultTemp()).thenReturn(Mono.just(result));
 
-        // when
         webTestClient.post().uri("/v1/result/temp")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(Result.class)
-                .isEqualTo(result);
-        // then
+                .expectBody(MatchDto.class)
+                .isEqualTo(matchDto);
+
         verify(registerResultUseCase, times(1)).createResultTemp();
     }
 }
