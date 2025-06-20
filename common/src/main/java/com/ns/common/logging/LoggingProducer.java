9d99f7777d80b43f2524336dfb20a9163cc71622
downfa11 <downfa11@naver.com>
2025-06-20 23:22:24 +0900

    feat: 공통 모듈(Common)에서 Kafka 기반 로깅 시스템 구축

--- common/src/main/java/com/ns/common/logging/LoggingProducer.java
+++ common/src/main/java/com/ns/common/logging/LoggingProducer.java
@@ -3,7 +3,9 @@ package com.ns.common.logging;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -12,12 +14,12 @@ public class LoggingProducer {
     @Value("${logging.topic}")
     private String topic;
 
-//    private final ReactiveKafkaProducerTemplate<String, LogMessage> loggingProducerTemplate;
-//
-//    public Mono<Void> sendMessage(String key, LogMessage value) {
-//            return loggingProducerTemplate.send(topic, key, value)
-//                    .doOnSuccess(result -> log.info("logging success: key={}, value={}", key, value))
-//                    .doOnError(error -> log.error("logging failed: key={}, value={}, error={}", key, value, error.getMessage()))
-//                    .then();
-//    }
+    private final ReactiveKafkaProducerTemplate<String, LogMessage> loggingProducerTemplate;
+
+    public Mono<Void> sendMessage(String key, LogMessage value) {
+            return loggingProducerTemplate.send(topic, key, value)
+                    .doOnSuccess(result -> log.info("logging success: key={}, value={}", key, value))
+                    .doOnError(error -> log.error("logging failed: key={}, value={}, error={}", key, value, error.getMessage()))
+                    .then();
+    }
 }
