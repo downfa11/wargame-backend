d2bbc6840c8a3671278530f44fd1c08d54cf355e
downfa11 <downfa11@naver.com>
2025-06-30 17:23:31 +0900

    refactor: vault, redis 연결 Configuration 수정

--- player/src/main/java/com/ns/player/config/RedissonConfig.java
+++ player/src/main/java/com/ns/player/config/RedissonConfig.java
@@ -16,16 +16,13 @@ public class RedissonConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.data.redis.password}")
-    private String redisPassword;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
                 .setAddress("redis://" + redisHost + ":" + redisPort)
-                .setPassword(redisPassword)
-                .setDatabase(0);
+                .setDatabase(1);
         return Redisson.create(config);
     }
 }
